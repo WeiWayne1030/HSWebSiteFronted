70f63b81e7d4337bf3b07d1ca9a49672bbace0e7
Wayne Sun <wayne10032004@gmail.com>
2023-09-01 12:33:57 +0800

    feat:buildOrder function

--- src/apis/order.js
+++ src/apis/order.js
@@ -14,3 +14,11 @@ export const buildOrderAPI = ({ shipName, address, shipTel, MethodId }) => {
     headers: { Authorization: `Bearer ${getToken()}` }
   })
 }
+
+export const getOrdersAPI = () =>{
+  // const searchParams = new URLSearchParams({ MethodId, state, orderNumber })
+  return request({
+    url: `/orders`,
+    headers: { Authorization: `Bearer ${getToken()}` }
+  }) 
+}
--- src/views/Order/OrderPage.vue
+++ src/views/Order/OrderPage.vue
@@ -1,29 +1,31 @@
 <template>
   <div>
-    <!-- header,footer,nav -->
     <LayoutNav />
     <LayoutHeader />
     <div class="build-order">
       <h2>建立訂單</h2>
-      <el-form ref="receiverForm" :model="receiverForm" :rules="rules" label-width="100px">
-        <el-form-item label="收貨人名稱">
-          <el-input v-model="receiverForm.name"></el-input>
+      <el-form :model="receiverForm" :rules="rules" label-width="100px" :ref="formRef">
+        <el-form-item label="收貨人名稱" prop="shipName">
+          <el-input v-model="receiverForm.shipName"></el-input>
         </el-form-item>
-        <el-form-item label="付款方式">
-          <el-select v-model="method" placeholder="請選擇付款方式">
+        <el-form-item label="付款方式" prop="method">
+          <el-select v-model="receiverForm.method" placeholder="請選擇付款方式">
             <el-option
-              v-for="item in paymentOptions"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value"
+              v-for="method in methods"
+              :key="method.id"
+              :label="method.label"
+              :value="method.name"
             ></el-option>
           </el-select>
         </el-form-item>
-        <el-form-item label="收貨地址">
+        <el-form-item label="收貨地址" prop="address">
           <el-input v-model="receiverForm.address" type="textarea"></el-input>
         </el-form-item>
-        <el-form-item label="收件人電話">
+        <el-form-item label="收件人電話" prop="shipTel">
           <el-input v-model="receiverForm.shipTel" type="text"></el-input>
+        </el-form-item>
+        <el-form-item label="總金額">
+          <span>{{ totalAmount }}</span>
         </el-form-item>
         <el-form-item>
           <el-button type="primary" @click="addProduct">結帳</el-button>
@@ -35,57 +37,106 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue'
+import { buildOrderAPI, getOrdersAPI } from '@/apis/order'
+import { getCartsAPI } from '@/apis/cart'
+import { useRouter } from 'vue-router'
+import { ElMessage } from 'element-plus'
 
 const receiverForm = ref({
-  name: '',
+  shipName: '',
+  method: '',
   address: '',
   shipTel: '',
-});
+})
 
-const method = ref('')
+const methods = ref([])
 
-const paymentOptions = [
-  { value: 'option1', label: '貨到付款' },
-  { value: 'option2', label: 'LinePay付款' },
-  { value: 'option3', label: '轉帳付款' }];
+const formRef = ref(null)
+
+const router = useRouter()
+
+// Fetch payment methods
+const fetchPaymentMethods = async () => {
+  try {
+    const res = await getOrdersAPI()
+    if (res && res.methods) {
+      methods.value = res.methods
+    } else {
+      console.error('Invalid API response:', res)
+    }
+  } catch (error) {
+    console.error('Error fetching payment methods:', error)
+  }
+}
+
+onMounted(() => {
+  fetchPaymentMethods()
+})
+
+const totalAmount = ref(0)
+const pagination = ref('')
+
+// Fetch cart information
+const fetchCartInfo = async () => {
+  try {
+    const res = await getCartsAPI(pagination)
+    if (res && res.rows) {
+      const calculatedTotalAmount = res.rows.reduce((total, cartItem) => {
+        return total + cartItem.amount
+      }, 0)
+      totalAmount.value = calculatedTotalAmount
+    } else {
+      console.error('Invalid API response:', res)
+    }
+  } catch (error) {
+    console.error('Error fetching cart information:', error)
+  }
+}
+
+onMounted(() => {
+  fetchCartInfo()
+})
 
 const rules = {
   shipName: [
-    { required: true, message: '請輸入收貨人姓名', trigger: 'blur' }
-  ],
-  address: [
-    { required: true, message: '請輸入地址', trigger: 'blur' },
-  ],
-  shipTel:[
-    { required: true, message: '請輸入電話', trigger: 'blur' },
-    { type: Number, message: '電話號碼需為數字', trigger: 'blur'}
+    { required: true, message: '請輸入收貨人姓名', trigger: 'blur' },
   ],
   method: [
     { required: true, message: '請選擇一項付款方式', trigger: 'blur' },
   ],
-
-  
+  address: [
+    { required: true, message: '請輸入地址', trigger: 'blur' },
+  ],
+  shipTel: [
+    { required: true, message: '請輸入電話', trigger: 'blur' },
+  ],
 }
 
-const addProduct = () => {
-  // Logic to add product
-  console.log('新增收貨人資料', receiverForm.value);
-};
+const addProduct = async () => {
+  try {
+    const { shipName, method, address, shipTel } = receiverForm.value
+
+    // 找到選擇的methodId
+    const selectedMethod = methods.value.find(m => m.name === method)
+    const MethodId = selectedMethod ? selectedMethod.id : ''
+
+    await buildOrderAPI({ shipName, MethodId, address, shipTel })
+    ElMessage({ type: 'success', message: '訂單建立成功' })
+    router.replace({ path: '/' })
+  } catch (error) {
+    ElMessage({ type: 'error', message: '建立訂單失敗' })
+  }
+}
 </script>
 
 <style scoped>
-.add-product {
+.build-order {
   max-width: 800px;
   margin: 0 auto;
   padding: 20px;
 }
 
-.upload-demo {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
 
 @media screen and (max-width: 768px) {
   .add-product {
