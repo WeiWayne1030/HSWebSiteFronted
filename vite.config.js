1bb34599143fc3bb0614a370abe63a3a17cead33
Wayne Sun <wayne10032004@gmail.com>
2023-09-10 02:49:20 +0800

    fix:vite.config

--- vite.config.js
+++ vite.config.js
@@ -10,8 +10,7 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/',
-  publicPath: '/hsfont/',
+  base:'./',
   plugins: [
     vue(),
     //...
