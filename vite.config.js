f0b9123b8dd2f5911fc1b01ea8e934bdfa0f6915
Wayne Sun <wayne10032004@gmail.com>
2023-09-10 11:02:24 +0800

    test:deploy

--- vite.config.js
+++ vite.config.js
@@ -7,10 +7,13 @@ import AutoImport from 'unplugin-auto-import/vite'
 import  Component  from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
+// eslint-disable-next-line no-control-regex
+const INVALID_CHAR_REGEX = /[\x00-\x1F\x7F<>*#"{}|^[\]`;?:&=+$,]/g;
+const DRIVE_LETTER_REGEX = /^[a-z]:/i;
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'./',
+  base:'/',
   plugins: [
     vue(),
     //...
@@ -35,6 +38,22 @@ export default defineConfig({
         `
       }
     }
-  }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        // https://github.com/rollup/rollup/blob/master/src/utils/sanitizeFileName.ts
+        sanitizeFileName(name) {
+          const match = DRIVE_LETTER_REGEX.exec(name);
+          const driveLetter = match ? match[0] : '';
+          // substr 是被淘汰語法，因此要改 slice
+          return (
+            driveLetter +
+            name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, "")
+          );
+        },
+      },
+    },
+  },
 })
 
